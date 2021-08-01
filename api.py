f2cd526865b86dee197d4749fd3bf14e9a10dd13
TexturKA <bob_impressive@mail.ru>
2021-08-01 10:48:28 +0300

    Исправил отступы, убрал повторяющиеся функции, удалил закомментированные участки кода

--- api.py
+++ api.py
@@ -3,6 +3,7 @@ import json
 from tinydb import TinyDB
 import jsonschema
 
+
 SCHEMA = {
         'type': 'object',
         'properties': {
@@ -29,6 +30,7 @@ SCHEMA = {
 db = TinyDB('db.json')
 app = Flask(__name__)
 
+
 def type_define(value):
     supported_types = ['date', 'phone', 'email', 'text']
     for t in supported_types:
@@ -43,6 +45,7 @@ def type_define(value):
 def to_json(data):
     return json.dumps(data) + "\n"
 
+
 def resp(code, data):
     return Response(
         status=code,
@@ -50,13 +53,11 @@ def resp(code, data):
         response=to_json(data)
     )
 
-@app.errorhandler(400)
-def page_not_found(e):
-    return resp(400, {})
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return resp(400, {})
+    return resp(404, {})
+
 
 @app.route('/get_form', methods=['POST'])
 def post_themes():
@@ -71,7 +72,6 @@ def post_themes():
                 count -= 1
             else:
                 break
-        # print(item, count)
         if count == 0:
             name = item['name']
             break
@@ -83,6 +83,7 @@ def post_themes():
         return resp(200, res)
     else:
         return resp(200, name)
+
 
 if __name__ == '__main__':
     app.debug = True
